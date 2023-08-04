eb33f5f9f9286928240e2a0eabf189b19e38cf24
Louis Brunner <louis.brunner.fr@gmail.com>
2023-08-04 18:36:17 +0100

    fix: format correctly

--- packages/ethers-signers-browser/build.rs
+++ packages/ethers-signers-browser/build.rs
@@ -11,14 +11,16 @@ async fn build_frontend(path: &str) -> Result<(), Box<dyn std::error::Error>> {
             public_url: Some("/dist/".to_string()),
             target: Some(Path::new(path).join("index.html").to_path_buf()),
             dist: Some(
-                Path::new(std::env::var("OUT_DIR").expect("OUT_DIR not set").as_str()).join("frontend").to_path_buf(),
+                Path::new(std::env::var("OUT_DIR").expect("OUT_DIR not set").as_str())
+                    .join("frontend")
+                    .to_path_buf(),
             ),
             ..ConfigOptsBuild::default()
         },
     }
     .run(None)
     .await?;
-  Ok(())
+    Ok(())
 }
 
 #[tokio::main]
@@ -26,12 +28,16 @@ async fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
     match build_frontend(FRONTEND).await {
-      Err(e) => {
-        eprintln!("Failed to build frontend, fallback to versionned: {}", e);
-        // FIXME: we shouldn't assume the frontend will have the same version
-        let frontend_vers = format!("{}-{}", FRONTEND, std::env::var("CARGO_PKG_VERSION").expect("CARGO_PKG_VERSION not set"));
-        build_frontend(&frontend_vers).await.unwrap();
-      },
-      _ => {},
+        Err(e) => {
+            eprintln!("Failed to build frontend, fallback to versionned: {}", e);
+            // FIXME: we shouldn't assume the frontend will have the same version
+            let frontend_vers = format!(
+                "{}-{}",
+                FRONTEND,
+                std::env::var("CARGO_PKG_VERSION").expect("CARGO_PKG_VERSION not set")
+            );
+            build_frontend(&frontend_vers).await.unwrap();
+        }
+        _ => {}
     };
 }
